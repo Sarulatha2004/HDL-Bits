2835714c8ce2f9efbea3690a5d3775515cc9f3fc
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-13 20:16:11 +0530

    Create 3.If statement.v

---
+++ Verilog Language/Procedures/3.If statement.v
@@ -0,0 +1,22 @@
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always   );
+    always @(*) begin
+        if(sel_b1)
+            if(sel_b2)
+                out_always=b;
+            else
+                out_always=a;
+        else
+            if(sel_b2)
+                out_always=a;
+            else
+                out_always=a;
+    end
+    assign out_assign=(sel_b1)?((sel_b2)?b:a):((sel_b2)?a:a);
+
+endmodule
