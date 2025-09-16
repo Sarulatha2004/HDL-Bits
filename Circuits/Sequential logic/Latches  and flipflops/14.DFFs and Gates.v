6ed15c94b1098aceea22d553a273211fb7df2c5b
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-16 17:52:40 +0530

    Create 14.DFFs and Gates.v

---
+++ Circuits/Sequential logic/Latches  and flipflops/14.DFFs and Gates.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input x,
+    output z
+); 
+    wire q1,q2,q3;
+    always @(posedge clk)
+        begin
+         q1<=x^q1;
+         q2<=x&~	q2;
+         q3<=x|~q3;
+        end
+         assign z=~(q1|q2|q3);
+        
+endmodule
