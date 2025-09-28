8d720e2fb63c32975de02b66daf7d70b69623f3f
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-28 19:08:13 +0530

    Create 101.Decade Counter again.v

---
+++ Circuits/Sequential logic/Counters/101.Decade Counter again.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,
+    output [3:0] q);
+    always @ (posedge clk)
+        begin
+            if(reset)
+                q<=1;
+            else
+                begin
+                    if(q<10)
+                        q<=q+1;
+                    else
+                        q<=1;
+                end
+        end
+
+endmodule
