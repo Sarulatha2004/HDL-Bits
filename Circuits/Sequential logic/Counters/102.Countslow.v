ab5b44f90dee3d047d9bf303015e7b05059eb159
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-28 19:13:40 +0530

    Create 102.Countslow.v

---
+++ Circuits/Sequential logic/Counters/102.Countslow.v
@@ -0,0 +1,25 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always @ (posedge clk)
+        begin
+            if(reset)
+                q<=0;
+            else
+                begin
+                  if(~slowena)
+                     q<=q;
+                  else
+                     begin
+                        if(q<9)
+                          q<=q+1;
+                        else
+                          q<=0;
+                     end
+               end
+       end
+
+endmodule
