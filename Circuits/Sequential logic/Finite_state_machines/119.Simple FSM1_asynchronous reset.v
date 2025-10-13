b042044eb3068821f3df5602af473500abf65917
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-13 10:34:46 +0530

    Create 119.Simple FSM1_asynchronous reset.v

---
+++ Circuits/Sequential logic/Finite_state_machines/119.Simple FSM1_asynchronous reset.v
@@ -0,0 +1,29 @@
+module top_module(
+    input clk,
+    input areset,   
+    input in,
+    output out);
+
+    parameter A=0, B=1; 
+    reg state, next_state;
+
+    always @(*) begin    
+        case(state)
+            A:next_state<= in ? A:B;
+            B:next_state <= in ? B:A;
+            default:next_state <=B;
+        endcase
+       
+       
+    end
+ assign out = state == B;
+    always @(posedge clk, posedge areset) begin
+        
+         if(areset)
+            state <= B;
+        else
+            state <= next_state;
+    end
+
+
+endmodule
