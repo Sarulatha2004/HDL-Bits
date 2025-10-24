3f1bbc467ed5e02e77e88c4fcfe20ee0239130bd
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-24 21:45:08 +0530

    Create 148.FSM.v

---
+++ Circuits/Sequential logic/Finite_state_machines/148.FSM.v
@@ -0,0 +1,37 @@
+module top_module (
+    input clk,
+    input reset,   // Synchronous active-high reset
+    input w,
+    output z
+);
+    
+    reg [2:0] state, next_state;
+    parameter A=0;
+    parameter B=1;
+    parameter C=2;
+    parameter D=3;
+    parameter E=4;
+    parameter F=5;
+    
+    always @ (posedge clk)
+        begin
+            if(reset)
+                state <= A;
+            else 
+                state <= next_state;
+        end
+    always @(*)
+        begin
+            case (state)
+                A:next_state <= w ? B:A;
+                B:next_state <= w ? C:D;
+                C:next_state <= w ? E:D;
+                D:next_state <= w ? F:A;
+                E:next_state <= w ? E:D;
+                F:next_state <= w ? C:D;
+                default:next_state <=A;
+            endcase
+        end
+    assign z= (state ==E) || (state == F);	
+
+endmodule
