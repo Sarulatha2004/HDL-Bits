d60c19c903f5d0f5e2c16a0eed067aa9ec5e20c8
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-24 17:15:09 +0530

    Create 141.Serial Two's Complementer(Mealy FSM).v

---
+++ Circuits/Sequential logic/Finite_state_machines/141.Serial Two's Complementer(Mealy FSM).v
@@ -0,0 +1,35 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    reg [1:0]state,next_state;
+    parameter A=0;
+    parameter B=1;
+    always @(posedge clk or posedge areset)
+        begin
+            if(areset)
+                state <= A;
+            else
+                state <= next_state;
+        end
+    always @(x or state)
+        begin
+            case(state)
+                A:begin
+                    next_state <= x ? B:A;
+                    z=x;
+                end
+                B:begin
+                    next_state <=B;
+                    z=~x;
+                end
+                default:next_state <=A;
+            endcase
+        end
+
+
+endmodule
+
+
