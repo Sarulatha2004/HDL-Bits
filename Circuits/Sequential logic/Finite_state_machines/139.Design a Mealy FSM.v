5526be1e08d57af5dceb7e37751eadffa72522a4
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-24 16:37:51 +0530

    Create 139.Design a Mealy FSM.v

---
+++ Circuits/Sequential logic/Finite_state_machines/139.Design a Mealy FSM.v
@@ -0,0 +1,31 @@
+module top_module (
+    input clk,
+    input aresetn,    
+    input x,
+    output z ); 
+    
+    reg [1:0] state,next_state;
+    
+    parameter S0=0;
+    parameter S1=1;
+    parameter S2=2;
+    
+    always @(posedge clk or negedge aresetn)
+        begin
+          if(!aresetn)
+            state <= S0;
+          else
+              state <= next_state;
+        end
+    always @(x or state)
+        begin
+            case(state)
+                S0: next_state <= x?S1:S0;
+                S1:next_state  <= x?S1:S2;
+                S2:next_state <= x? S1:S0;
+                default :next_state <= S0;
+            endcase
+        end
+    assign z= x==1 && state == S2; 
+
+endmodule
