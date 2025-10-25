b38d14d533f35ecf935c727604279aff6dd46157
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-25 10:14:37 +0530

    Update 131.Lemmings4.v

--- Circuits/Sequential logic/Finite_state_machines/131.Lemmings4.v
+++ Circuits/Sequential logic/Finite_state_machines/131.Lemmings4.v
@@ -45,14 +45,14 @@ module top_module(
     always @(*)
         begin
             case(state)
-                LEFT:next_state <= ground ? (dig ?DIG_LEFT:(bump_left ? RIGHT:LEFT)) : FALL_LEFT;
-                RIGHT:next_state <= ground ? (dig ? DIG_RIGHT:(bump_right ? LEFT:RIGHT)): FALL_RIGHT;
-                FALL_LEFT:next_state <=ground ? (count >= 20 ? SPLATTER :LEFT):FALL_LEFT;
-                FALL_RIGHT:next_state <= ground ? (count >= 20 ? SPLATTER :RIGHT) : FALL_RIGHT;
-                DIG_LEFT:next_state <= ground ? DIG_LEFT: FALL_LEFT;
-                DIG_RIGHT :next_state <= ground ? DIG_RIGHT : FALL_RIGHT;
-                SPLATTER :next_state <= SPLATTER;
-                default:next_state <= LEFT;
+                LEFT:next_state = ground ? (dig ?DIG_LEFT:(bump_left ? RIGHT:LEFT)) : FALL_LEFT;
+                RIGHT:next_state = ground ? (dig ? DIG_RIGHT:(bump_right ? LEFT:RIGHT)): FALL_RIGHT;
+                FALL_LEFT:next_state =ground ? (count >= 20 ? SPLATTER :LEFT):FALL_LEFT;
+                FALL_RIGHT:next_state = ground ? (count >= 20 ? SPLATTER :RIGHT) : FALL_RIGHT;
+                DIG_LEFT:next_state = ground ? DIG_LEFT: FALL_LEFT;
+                DIG_RIGHT :next_state = ground ? DIG_RIGHT : FALL_RIGHT;
+                SPLATTER :next_state = SPLATTER;
+                default:next_state = LEFT;
             endcase
         end
     assign walk_left =( state == SPLATTER) ? 1'b0:(state == LEFT);
