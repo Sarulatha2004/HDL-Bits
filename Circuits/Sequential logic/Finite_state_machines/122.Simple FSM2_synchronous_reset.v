80b9ce71cbc864af6987bbde41161ff3f00488ed
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-25 10:11:16 +0530

    Update 122.Simple FSM2_synchronous_reset.v

--- Circuits/Sequential logic/Finite_state_machines/122.Simple FSM2_synchronous_reset.v
+++ Circuits/Sequential logic/Finite_state_machines/122.Simple FSM2_synchronous_reset.v
@@ -12,7 +12,7 @@ module top_module(
         case(state)
             OFF:next_state = j? ON:OFF;
             ON :next_state = k? OFF:ON;
-            default: next_state <= OFF;
+            default: next_state = OFF;
         endcase
     end
     
