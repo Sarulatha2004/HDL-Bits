7a99bd657de4db8bd233794a67cbf155ae6a3bcf
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-25 10:18:18 +0530

    Update 143.FSM.v

--- Circuits/Sequential logic/Finite_state_machines/143.FSM.v
+++ Circuits/Sequential logic/Finite_state_machines/143.FSM.v
@@ -22,12 +22,12 @@ module top_module (
     always @(x or state)
         begin
             case(state)
-                 S0: next_state <= x? S1: S0;
-                 S1: next_state <= x? S4: S1;
-                 S2: next_state <= x? S1: S2;
-                 S3: next_state <= x? S2: S1;
-                 S4: next_state <= x? S4: S3;
-                default:next_state <= S0;
+                 S0: next_state = x? S1: S0;
+                 S1: next_state = x? S4: S1;
+                 S2: next_state = x? S1: S2;
+                 S3: next_state = x? S2: S1;
+                 S4: next_state = x? S4: S3;
+                default:next_state = S0;
             endcase
         end
     assign z=(state == S3) || (state == S4);
